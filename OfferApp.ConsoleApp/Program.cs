582dd6dd3fba6ba748e50e73d7048a4b80318914
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-06-29 23:46:43 +0200

    [Module5] Task 2 Solution

--- OfferApp.ConsoleApp/Program.cs
+++ OfferApp.ConsoleApp/Program.cs
@@ -1,6 +1,11 @@
 ﻿using OfferApp.ConsoleApp;
 using OfferApp.Core;
 
+// Wykorzystałem wzorzec strategię.
+// Każdy widok implementuje interfejs IConsoleView.
+// Następnie wewnątrz BidInteractionService tworzę co wywołanie kolekcję IConsoleView.
+// Szczegóły w klasie BidInteractionService
+
 var menuService = Extensions.CreateMenuService();
 var bidService = Extensions.CreateBidService();
 var bidInteractionService = new BidInteractionService(menuService, bidService);
